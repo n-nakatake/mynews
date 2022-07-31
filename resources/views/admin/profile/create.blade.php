33bedcadfdaa895ffd9bba94e1bd9d7dfbde3c87
n-nakatake <nakatake@iroiro-inc.com>
2022-07-31 17:29:28 +0900

    タイトルの修正

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,9 +5,19 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews</title>
+        <title>MyProfile</title>
     </head>
     <body>
         <h1>Myプロフィール</h1>
+        <div>
+       　　  <ul>
+                <li>名前</li>
+                <li>生年月日</li>
+                <li>郵便番号</li>
+                <li>住所</li>
+                <li>電話番号</li>
+   　　       </ul>
+        </div>        
+        
     </body>
 </html>
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews</title>
+        <title>MyProfile</title>
     </head>
     <body>
         <h1>Myプロフィール編集画面</h1>
