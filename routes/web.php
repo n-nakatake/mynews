1b286c7d518ec7798ab34f2cfca0b887591ec764
n-nakatake <nakatake@iroiro-inc.com>
2022-08-10 19:54:42 +0900

    Laravel 12 ユーザー認証を実装する

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,9 @@ Route::get('/', function () {
 Route::get('XXX', 'AAAController@bbb');
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    // 課題4
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');    
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');;
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');;    
     
 });
 
@@ -30,3 +29,7 @@ Route::group(['prefix' => 'admin'], function() {
 
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
