e14be3628fdb1cfd93f38ec0298abd95ebcef324
n-nakatake <nakatake@iroiro-inc.com>
2022-08-14 15:35:05 +0900

    Laravel14課題途中

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,20 +1,32 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
-    //
     public function add()
     {
         return view('admin.profile.create');
     }
 
-    public function creat()
+    public function create(Request $request)
     {
+        // Varidationを行う
+        $this->validate($request, Profile::$rules);
+        $Profile = new Profile;
+        $form = $request->all();
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+
+        // データベースに保存する
+        $Profile->fill($form);
+        $Profile->save();
+
         return redirect('admin/profile/create');
     }
 
